bb4a369826d31fb31c0ecbb915e70e01e97b1a07
Alberto Corpas <alberto.corpas@gmail.com>
2026-09-03 11:18:51 +0200

    Prepare CLARISENDA website for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+
+  site: 'https://clarisenda.com',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [
+    sitemap({
+      filter: (page) =>
+        page !== 'https://clarisenda.com/'
+    })
+  ]
+
 });
